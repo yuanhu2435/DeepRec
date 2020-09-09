c0a229a706a7cc0aa16600f3a4bd626f7a99cc14
yunfeima <yunfei.mao@intel.com>
2020-09-09 10:37:11 +0800

    Fix bugs in disable eigen fusion

--- tensorflow/core/grappler/optimizers/remapper.cc
+++ tensorflow/core/grappler/optimizers/remapper.cc
@@ -627,6 +627,7 @@ bool FindContractionWithBiasInPort(const RemapperContext& ctx,
 bool FindContractionWithBiasAddAndAdd(const RemapperContext& ctx,
                                       const utils::MutableNodeView& node_view,
                                       ContractionWithBiasAddAndAdd* matched) {
+  if (DisableMKL()) return false;
   // Fusion with AddN is supported only when it has two inputs.
   // TODO(lyandy): Forward controls for patterns with control dependencies.
   if (HasControlFaninOrFanout(node_view) || node_view.NumRegularFanins() != 2)
@@ -677,6 +678,7 @@ bool FindContractionWithBiasAddAndAdd(const RemapperContext& ctx,
 bool FindContractionWithBiasAndAddActivation(
     const RemapperContext& ctx, int node_index,
     ContractionWithBiasAndAddActivation* matched) {
+  if (DisableMKL()) return false;
   const auto* node_view = ctx.graph_view.GetNode(node_index);
   // TODO(lyandy): Forward controls for patterns with control dependencies.
   if (HasControlFaninOrFanout(*node_view)) return false;
@@ -1669,9 +1671,6 @@ Status Remapper::Optimize(Cluster* cluster, const GrapplerItem& item,
     }
 
 #ifdef INTEL_MKL
-    if (DisableMKL()) {
-      continue;
-    }
     ContractionWithBiasAddAndAdd contract_with_bias_and_add;
     ContractionWithBiasAndAddActivation contract_with_bias_and_add_activation;
 
