271c7e129e4874f0b9644facd090a47424caebe7
marvinYu <weifei.yu@intel.com>
2022-01-05 15:21:43 +0800

    update fc_test

--- tensorflow/python/feature_column/feature_column_v2_test.py
+++ tensorflow/python/feature_column/feature_column_v2_test.py
@@ -40,7 +40,7 @@ from tensorflow.python.summary.writer import writer
 
 def _initialized_session(graph=None, config=None):
   sess = session.Session(graph=graph, config=config)
-  writer.FileWriter('/root/DeepRec/graph_fusion', sess.graph)
+  # writer.FileWriter('/root/DeepRec/graph_fusion', sess.graph)
   sess.run(variables_lib.global_variables_initializer())
   sess.run(lookup_ops.tables_initializer())
   return sess
@@ -57,45 +57,6 @@ def print_ops(sess, op_name):
     print(_output)
     print(sess.run(_output))
   print("<" * 64, "\n")
-
-
-@ops.RegisterGradient("FusedSafeEmbeddingLookupSparse")
-def _embedding_grad(op, grad):
-
-    # Reshape_2_grad
-    # _input0_shape = tf.shape(_reshape_2_shape_input_tensor)
-    # _reshape_grad = tf.reshape(grad, _input0_shape)
-    
-    # # Select Grad
-    # _zeros_like = tf.zeros_like(_zero_like_input_tensor)
-    # _select_grad = tf.where(_tile_input_tensor, _zeros_like, _reshape_grad)
-
-    # # SparseSegmentMeanGrad
-    # dim0 = tf.shape(_gather_input_tensor)[0]
-    # _sparse_segment_mean_grad = math_ops.sparse_segment_mean_grad(_select_grad, _unique_1_input_tensor, _cast_input_tensor, dim0)
-
-    # # GatherGrad
-    # params = _weight_input_tensor
-    # with ops.colocate_with(params):
-    #     params_shape = array_ops.shape(params, out_type=ops.dtypes.int64)
-    #     params_shape = math_ops.cast(params_shape, dtypes.int32)
-
-    # # Build appropriately shaped IndexedSlices
-    # indices = _unique_0_input_tensor
-    # size = array_ops.expand_dims(array_ops.size(indices), 0)
-    # values_shape = array_ops.concat([size, params_shape[1:]], 0)
-    # with warnings.catch_warnings():
-    #     warnings.filterwarnings(
-    #         "ignore",
-    #         message="Converting sparse IndexedSlices to a dense Tensor.*")
-    #     values = array_ops.reshape(_sparse_segment_mean_grad, values_shape)
-    # indices = array_ops.reshape(indices, size)
-    # return [ops.IndexedSlices(values, indices, params_shape), None]
-
-    print(">" * 10, grad)
-    grad, unique_value = fused_embedding_ops.fused_safe_embedding_lookup_sparse_grad(gradients=grad, input=op.inputs[1], dense_shape=op.inputs[2], indices=op.inputs[3])
-    return [ops.IndexedSlices(grad, unique_value, op.inputs[2]), None, None, None, None]
-    # return [grad, unique_value, None, None, None]
 
 
 class EmbeddingColumnTest(test.TestCase):
