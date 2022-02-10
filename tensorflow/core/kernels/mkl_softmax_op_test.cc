b312548d4723f0e7a4b3fe95b151557ec781b920
jmaksymc <jakub.maksymczuk@intel.com>
2022-02-10 04:47:16 +0100

    [OP] Add oneDNN Softmax test. (#5)

--- tensorflow/core/kernels/mkl_softmax_op_test.cc
+++ tensorflow/core/kernels/mkl_softmax_op_test.cc
@@ -14,32 +14,209 @@ limitations under the License.
 ==============================================================================*/
 
 #ifdef INTEL_MKL
-
 #include <functional>
 #include <vector>
-
-#include "tensorflow/core/common_runtime/device.h"
-#include "tensorflow/core/common_runtime/device_factory.h"
+#include "mkldnn.hpp"
+#include "absl/strings/match.h"
+#include "tensorflow/cc/ops/const_op.h"
+#include "tensorflow/cc/ops/nn_ops.h"
+#include "tensorflow/cc/ops/array_ops.h"
+#include "tensorflow/cc/ops/array_ops_internal.h"
+#include "tensorflow/cc/ops/standard_ops.h"
 #include "tensorflow/core/common_runtime/kernel_benchmark_testlib.h"
-#include "tensorflow/core/framework/allocator.h"
 #include "tensorflow/core/framework/fake_input.h"
 #include "tensorflow/core/framework/node_def_builder.h"
-#include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/tensor.h"
-#include "tensorflow/core/framework/tensor_testutil.h"
-#include "tensorflow/core/framework/types.h"
 #include "tensorflow/core/framework/types.pb.h"
-#include "tensorflow/core/graph/node_builder.h"
-#include "tensorflow/core/graph/testlib.h"
 #include "tensorflow/core/kernels/ops_testutil.h"
 #include "tensorflow/core/kernels/ops_util.h"
+#include "tensorflow/core/platform/cpu_info.h"
+#include "tensorflow/core/platform/env.h"
+#include "tensorflow/core/platform/stacktrace_handler.h"
+#include "tensorflow/core/platform/str_util.h"
 #include "tensorflow/core/platform/test.h"
 #include "tensorflow/core/platform/test_benchmark.h"
-#include "tensorflow/core/public/session_options.h"
-#include "tensorflow/core/public/version.h"
+#include "tensorflow/core/public/session.h"
+#include "tensorflow/core/framework/fake_input.h"
 #include "tensorflow/core/util/mkl_util.h"
+#include <gtest/gtest.h>
 
 namespace tensorflow {
+
+//----------------------------------------------------------------------------//
+// Softmax Unit Tests are below.                                              //
+//----------------------------------------------------------------------------//
+
+namespace MKLSoftmaxTestDefs {
+    typedef std::tuple<
+    DataType,             // input_type
+    std::vector<long long int>
+    > SoftmaxTestParams;
+    std::vector<DataType> dataTypes {
+        DataType::DT_FLOAT,
+        DataType::DT_BFLOAT16
+    };
+    std::vector<std::vector<long long int>> SIZES_2D = {{64, 64}, {1,1}, {1,32}, {32, 21}};
+    std::vector<std::vector<long long int>> SIZES_3D = {{32, 16, 1}, {32, 32, 32}, {128, 128, 128}, {1, 1, 1}};
+    std::vector<std::vector<long long int>> SIZES_4D = {{32, 32, 32, 32}, {16, 1, 1, 1}, {31, 63, 15, 7}};
+} // namespace MKLSoftmaxTestDefs
+
+using namespace MKLSoftmaxTestDefs;
+class SoftmaxTestBase :
+    public ::testing::WithParamInterface<MKLSoftmaxTestDefs::SoftmaxTestParams>,
+    public OpsTestBase {
+ private:
+    // Test definition (straight from Params, filled in SetUp)
+    DataType input_type;
+    std::vector<long long int> input_size;
+    // Test input Tensors (filled in SetUp)
+    Tensor input;
+    Tensor zeros;
+    // Test output Tensors (filled in Run method)
+    Tensor mkl_values;
+    Tensor default_values;
+
+    static void RunAndFetch(const tensorflow::Scope& root, const string& fetch,
+                          Tensor* output) {
+    tensorflow::GraphDef graph;
+    TF_ASSERT_OK(root.ToGraphDef(&graph));
+    std::unique_ptr<tensorflow::Session> session(
+        tensorflow::NewSession(tensorflow::SessionOptions()));
+    TF_ASSERT_OK(session->Create(graph));
+    std::vector<Tensor> unfused_tensors;
+    TF_ASSERT_OK(session->Run({}, {fetch}, {}, &unfused_tensors));
+    *output = unfused_tensors[0];
+    }
+
+    void runDefault() {
+          auto root = tensorflow::Scope::NewRootScope();
+          auto input_op =
+              ops::Const(root.WithOpName("input"), Input::Initializer(input));
+          Output next_op = ops::Softmax(root.WithOpName("softmax"), input_op);
+          string last_op = "softmax";
+          RunAndFetch(root, last_op, &default_values);
+    };
+
+    void runMkl() {
+    TF_EXPECT_OK(
+        NodeDefBuilder("mkl_softmax_op", "_MklSoftmax") //build node
+            .Input(FakeInput(input_type))
+	    .Input(FakeInput(DT_UINT8))
+            .Attr("_kernel", "MklLayoutDependentOp")
+            .Finalize(node_def()));
+    TF_EXPECT_OK(InitOp()); //initial
+    switch(input_type) {
+        case DT_FLOAT:
+            AddInputFromArray<float>(input.shape(), input.flat<float>()); // input
+            break;
+        case DT_BFLOAT16:
+            AddInputFromArray<Eigen::bfloat16>(input.shape(), input.flat<Eigen::bfloat16>()); // input
+            break;
+        default:
+            GTEST_FAIL() << "Unexpected DataType";
+    }
+    AddInputFromArray<uint8_t>(zeros.shape(), zeros.flat<uint8_t>()); // input
+    TF_EXPECT_OK(RunOpKernel()); //Run the node computation
+    mkl_values = *GetOutput(0); //Get output
+    }
+ public:
+    static std::string getTestCaseName(::testing::TestParamInfo<SoftmaxTestParams> obj) {
+        DataType input_type;
+        std::vector<long long int> input_size;
+        std::tie(input_type, input_size) = obj.param;
+        std::ostringstream result;
+        result << "Softmax_Type_";
+        switch(input_type) {
+            case DataType::DT_FLOAT:
+                result << "FLOAT";
+                break;
+            case DataType::DT_BFLOAT16:
+                result << "BFLOAT16";
+                break;
+            default:
+                result << "UNRECOGNISED_TYPE";
+        }
+        result << "_Sizes";
+        for (auto &x : input_size) {
+            result << "_" << x;
+        }
+        return result.str();
+    }
+
+    void SetUp() {
+        std::tie(input_type, input_size) = this->GetParam();
+        input = Tensor(input_type, TensorShape(tensorflow::gtl::ArraySlice<long long int>(input_size.data(), input_size.size())));
+        switch(input_type) {
+            case DT_FLOAT:
+                input.flat<float>() = input.flat<float>().template setRandom<Eigen::internal::NormalRandomGenerator<float>>(); // input
+                break;
+            case DT_BFLOAT16:
+                input.flat<Eigen::bfloat16>() = input.flat<Eigen::bfloat16>().template setRandom<Eigen::internal::UniformRandomGenerator<Eigen::bfloat16>>(); // input
+		        input.flat<Eigen::bfloat16>() = input.flat<Eigen::bfloat16>() - input.flat<Eigen::bfloat16>().constant((Eigen::bfloat16)0.5);
+		        input.flat<Eigen::bfloat16>() = input.flat<Eigen::bfloat16>() * input.flat<Eigen::bfloat16>().constant((Eigen::bfloat16)200.0);
+                break;
+            default:
+                GTEST_FAIL() << "Unexpected DataType";
+        }
+        zeros = Tensor(DT_UINT8, TensorShape({64, 64}));
+        auto zeros_mapped = zeros.tensor<uint8_t, 2>();
+        for(int i = 0; i < 64; i++){
+            for(int j = 0; j < 64; j++){
+                zeros_mapped(i, j) = 0;
+            }
+        }
+    }
+
+    void Run() {
+        runDefault();
+        runMkl();
+    }
+
+    void Validate() {
+        switch(input_type) {
+            case DT_FLOAT:
+                ASSERT_EQ(default_values.dtype(), mkl_values.dtype());
+                ASSERT_EQ(default_values.shape(), mkl_values.shape());
+                test::ExpectClose(default_values, mkl_values, 1e-7);
+                break;
+            case DT_BFLOAT16:
+                ASSERT_EQ(default_values.dtype(), mkl_values.dtype());
+                ASSERT_EQ(default_values.shape(), mkl_values.shape());
+                test::ExpectClose(default_values, mkl_values, 1e-4);
+                break;
+            default:
+            GTEST_FAIL() << "Unexpected DataType";
+        }
+    }
+};
+
+TEST_P(SoftmaxTestBase, CompareWithRefs) {
+    SetUp();
+    Run();
+    Validate();
+};
+
+INSTANTIATE_TEST_CASE_P(Softmax2D, SoftmaxTestBase,
+    ::testing::Combine(
+        ::testing::ValuesIn(dataTypes),
+        ::testing::ValuesIn(SIZES_2D)),
+    SoftmaxTestBase::getTestCaseName);
+
+INSTANTIATE_TEST_CASE_P(Softmax3D, SoftmaxTestBase,
+    ::testing::Combine(
+        ::testing::ValuesIn(dataTypes),
+        ::testing::ValuesIn(SIZES_3D)),
+    SoftmaxTestBase::getTestCaseName);
+
+INSTANTIATE_TEST_CASE_P(Softmax4D, SoftmaxTestBase,
+    ::testing::Combine(
+        ::testing::ValuesIn(dataTypes),
+        ::testing::ValuesIn(SIZES_4D)),
+    SoftmaxTestBase::getTestCaseName);
+
+//----------------------------------------------------------------------------//
+// Performance benchmarks are below.                                          //
+//----------------------------------------------------------------------------//
 
 template <typename T>
 static Graph* Softmax(const string& kind, const TensorShape& in_shape) {
