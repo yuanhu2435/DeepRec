904beeb32ea4ff77e600d110113c94a989bfae10
Duyi-Wang <duyi.wang@intel.com>
2022-03-01 17:00:59 +0800

    [Modelzoo] update template.py.

--- modelzoo/template.py
+++ modelzoo/template.py
@@ -12,22 +12,6 @@ CATEGORICAL_COLUMNS = ["C" + str(i) for i in range(1, 27)]  # 1-26 inclusive
 LABEL_COLUMN = ["clicked"]
 
 
-def add_layer_summary(value, tag):
-    tf.summary.scalar('%s/fraction_of_zero_values' % tag,
-                      tf.nn.zero_fraction(value))
-    tf.summary.histogram('%s/activation' % tag, value)
-
-
-# generate dataset pipline
-def generate_input_data():
-    pass
-
-
-# generate feature columns
-def build_feature_cols():
-    pass
-
-
 class WDL():
     def __init__(self):
         self.label = None
@@ -40,6 +24,12 @@ class WDL():
         self.train_fn()
         self.metrics_fn()
 
+    # used to add summary in tensorboard
+    def add_layer_summary(self,value, tag):
+        tf.summary.scalar('%s/fraction_of_zero_values' % tag,
+                          tf.nn.zero_fraction(value))
+        tf.summary.histogram('%s/activation' % tag, value)
+
     # process input data
     def input_fn(self):
         pass
@@ -47,7 +37,8 @@ class WDL():
     # model structure
     # finally get a probability of predict
     def model_fn(self):
-        self.probability = None
+        self.output = None
+        self.add_layer_summary(self.output)
         pass
 
     # compute loss and define optimizer
@@ -59,6 +50,7 @@ class WDL():
     def metrics_fn(self):
         self.acc = None
         self.auc = None
+        self.add_layer_summary(self.acc)
         pass
 
     def train(self, session, timeline=False, tensorboard=False):
@@ -68,7 +60,7 @@ class WDL():
         if timeline:
             pass
 
-        session.run(self.probability, self.train_op)
+        session.run(self.train_op)
 
     def eval(self, session):
         self._is_training = False
@@ -80,11 +72,14 @@ class WDL():
         pass
 
 
-# Get parse
-def get_arg_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument()
-    return parser
+# generate dataset pipline
+def generate_input_data():
+    pass
+
+
+# generate feature columns
+def build_feature_cols():
+    pass
 
 
 def train(session, model, dataset, steps):
@@ -122,9 +117,6 @@ def main(tf_config=None, server=None):
     # set directory path for checkpoint_dir
     print('Checkpoint directory path')
 
-    # create feature column
-    feature_column = build_feature_cols()
-
     # create data pipline of train & test dataset
     train_dataset = generate_input_data()
     test_dataset = generate_input_data()
@@ -135,6 +127,9 @@ def main(tf_config=None, server=None):
 
     train_init_op = iterator.make_initializer(train_dataset)
     test_init_op = iterator.make_initializer(test_dataset)
+
+    # create feature column
+    feature_column = build_feature_cols()
 
     # create variable partitioner for distributed training
     num_ps_replicas = len(tf_config['ps_hosts']) if tf_config else 0
@@ -157,14 +152,35 @@ def main(tf_config=None, server=None):
         eval(sess, model, test_init_op, 'test_steps')
 
 
+# Get parse
+def get_arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument()
+    return parser
+
+
 # Parse distributed training configuration and generate cluster information
 def generate_cluster_info(TF_CONFIG):
     pass
+
+# Some DeepRec's features are enabled by ENV. 
+# This func is used to set ENV and enable these features.
+# A triple quotes comment is used to introduce these features and play an emphasizing role.
+def set_env_for_DeepRec():
+    '''
+    DNNL_MAX_CPU_ISA: Specify the highest instruction set used by oneDNN (when the version is less than 2.5.0), 
+        it will be set to AVX512_CORE_AMX to enable Intel CPU's feature.
+    '''
+    pass
+
 
 
 if __name__ == "__main__":
     parser = get_arg_parser()
     args = parser.parse_args()
+
+    if 'DeepRec':
+        set_env_for_DeepRec()
 
     TF_CONFIG = os.getenv('TF_CONFIG')
     if not TF_CONFIG:
